7a18a144161ff67f2bbf9427761bc88661aaf702
Zane <zane3412@qq.com>
2025-12-02 14:23:04 +0800

    first commit

---
+++ contracts/XFYToken/evXFYTokenPool.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.24;
+
+import {ITypeAndVersion} from "@chainlink/contracts/src/v0.8/shared/interfaces/ITypeAndVersion.sol";
+import {IBurnMintERC20} from "@chainlink/contracts/src/v0.8/shared/token/ERC20/IBurnMintERC20.sol";
+
+import {BurnMintTokenPoolAbstract} from "@chainlink/contracts-ccip/contracts/pools/BurnMintTokenPoolAbstract.sol";
+import {TokenPool} from "@chainlink/contracts-ccip/contracts/pools/TokenPool.sol";
+
+
+contract evXFYTokenPool is BurnMintTokenPoolAbstract, ITypeAndVersion {
+  string public constant override typeAndVersion = "BurnMintTokenPool 1.6.x-dev";
+
+  constructor(
+    IBurnMintERC20 token,
+    uint8 localTokenDecimals,
+    address[] memory allowlist,
+    address rmnProxy,
+    address router
+  ) TokenPool(token, localTokenDecimals, allowlist, rmnProxy, router) {}
+
+  /// @inheritdoc TokenPool
+  function _lockOrBurn(
+    uint256 amount
+  ) internal virtual override {
+    IBurnMintERC20(address(i_token)).burn(amount);
+  }
+}
